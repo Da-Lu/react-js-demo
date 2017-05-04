875a3099caf359dd82d19ea4511094bf11589c7e
Daniel Lu <dalu@usc.edu>
2017-05-03 18:11:17 -0700

    quick start demo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,393 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+// class Clock extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {date: new Date()};
+//   }
+//
+//   componentDidMount() {
+//     this.timerID = setInterval(
+//       () => this.tick(),
+//       1000
+//     );
+//   }
+//
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
+//
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, Daniel!</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+// }
+//
+// class Toggle extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {isToggleOn: true};
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//
+//   handleClick() {
+//     this.setState(prevState => ({
+//         isToggleOn: !prevState.isToggleOn
+//     }));
+//   }
+//
+//   render() {
+//     return (
+//       <button onClick={this.handleClick}>
+//         {this.state.isToggleOn ? 'ON' : 'OFF'}
+//       </button>
+//     );
+//   }
+// }
+//
+// function UserGreeting(props) {
+//   return <h1>Welcome back!</h1>;
+// }
+// function GuestGreeting(props) {
+//   return <h1>Please sign up.</h1>;
+// }
+// function Greeting(props) {
+//   const isLoggedIn = props.isLoggedIn;
+//   if (isLoggedIn) {
+//     return <UserGreeting />;
+//   }
+//   return <GuestGreeting />;
+// }
+//
+// function LoginButton(props) {
+//   return (
+//     <button onClick={props.onClick}>
+//       Login
+//     </button>
+//   );
+// }
+//
+// function LogoutButton(props) {
+//   return (
+//     <button onClick={props.onClick}>
+//       Logout
+//     </button>
+//   );
+// }
+//
+// class LoginControl extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleLoginClick = this.handleLoginClick.bind(this);
+//     this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//     this.state = {isLoggedIn: false};
+//   }
+//
+//   handleLoginClick() {
+//     this.setState({isLoggedIn: true});
+//   }
+//
+//   handleLogoutClick() {
+//     this.setState({isLoggedIn: false});
+//   }
+//
+//   render() {
+//     const isLoggedIn = this.state.isLoggedIn;
+//
+//     let button = null;
+//     if (isLoggedIn) {
+//       button = <LogoutButton onClick={this.handleLogoutClick} />;
+//     } else {
+//       button = <LoginButton onClick={this.handleLoginClick} />;
+//     }
+//
+//     return (
+//       <div>
+//         <Greeting isLoggedIn={isLoggedIn} />
+//         {button}
+//       </div>
+//     );
+//   }
+// }
+//
+// class NameForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: ''};
+//
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//
+//   handleChange(event) {
+//     this.setState({value: event.target.value.toUpperCase()});
+//   }
+//
+//   handleSubmit(event) {
+//     alert('A name was submitted: ' + this.state.value);
+//     event.preventDefault();
+//   }
+//
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <lavel>
+//           Name:
+//           <input type="text" value={this.state.value} onChange={this.handleChange} />
+//         </lavel>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
+//
+// class EssayForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: 'Please write an essay about your favorite DOM element.'};
+//
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//
+//   handleChange(event) {
+//     this.setState({value: event.target.value});
+//   }
+//
+//   handleSubmit(event) {
+//     alert('A essay was submitted: ' + this.state.value);
+//     event.preventDefault();
+//   }
+//
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <lavel>
+//           Name:
+//           <textarea value={this.state.value} onChange={this.handleChange} />
+//         </lavel>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
+//
+// function BoilingVerdict(props) {
+//   if (props.celsius >= 100) {
+//     return <p>The water would boil.</p>;
+//   }
+//   return <p>The water would not boil.</p>;
+// }
+//
+// class Calculator extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+//     this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+//     this.state = {temperature: '', scale: 'c'};
+//   }
+//   handleCelsiusChange(temperature) {
+//     this.setState({scale: 'c', temperature});
+//   }
+//   handleFahrenheitChange(temperature) {
+//     this.setState({scale: 'f', temperature});
+//   }
+//   render() {
+//     const scale = this.state.scale;
+//     const temperature = this.state.temperature;
+//     const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+//     const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+//     return (
+//       <div>
+//         <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange} />
+//         <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange}/>
+//         <BoilingVerdict celsius={parseFloat(celsius)} />
+//       </div>
+//     );
+//   }
+// }
+//
+// const scaleNames = {
+//   c: 'Celsius',
+//   f: 'Fahrenheit'
+// };
+// class TemperatureInput extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleChange = this.handleChange.bind(this);
+//     this.state = {temperature: ''};
+//   }
+//
+//   handleChange(e) {
+//     // this.setState({temperature: e.target.value});
+//     this.props.onTemperatureChange(e.target.value);
+//   }
+//
+//   render() {
+//     const temperature = this.props.temperature;
+//     const scale = this.props.scale;
+//     return (
+//       <fieldset>
+//         <legend>Enter temperature in {scaleNames[scale]}:</legend>
+//         <input value={temperature} onChange={this.handleChange}/>
+//       </fieldset>
+//     );
+//   }
+// }
+//
+// function toCelsius (fahrenheit) {
+//   return (fahrenheit - 32) * 5 / 9;
+// }
+// function toFahrenheit(celsius) {
+//   return (celsius * 9 / 5) + 32;
+// }
+// function tryConvert(temperature, convert) {
+//   const input = parseFloat(temperature);
+//   if (Number.isNaN(input)) {
+//     return '';
+//   }
+//   const output = convert(input);
+//   const rounded = Math.round(output * 1000) / 1000;
+//   return rounded.toString();
+// }
+
+class ProductCategoryRow extends Component {
+  render() {
+    return <tr><th colSpan="2">{this.props.category}</th></tr>;
+  }
+}
+
+class ProductRow extends Component {
+  render() {
+    var name = this.props.product.stocked ?
+      this.props.product.name :
+      <span style={{color: 'red'}}>
+        {this.props.product.name}
+      </span>
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductTable extends Component {
+  render() {
+    var rows = [];
+    var lastCategory = null;
+    this.props.products.forEach( (product) => {
+      if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+        return;
+      }
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+    this.handleInStockInputChange = this.handleInStockInputChange.bind(this);
+  }
+
+  handleFilterTextInputChange(e) {
+    this.props.onFilterTextInput(e.target.value);
+  }
+
+  handleInStockInputChange(e) {
+    this.props.onInStockInput(e.target.checked);
+  }
+
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." value={this.props.filterText} onChange={this.handleFilterTextInputChange}/>
+        <p>
+          <input type="checkbox" checked={this.props.inStockOnly} onChange={this.handleInStockInputChange}/>
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+    this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
+    this.handleInStockInput = this.handleInStockInput.bind(this);
+  }
+
+  handleFilterTextInput(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+
+  handleInStockInput(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} onFilterTextInput={this.handleFilterTextInput} onInStockInput={this.handleInStockInput}/>
+        <ProductTable products={this.props.products} filterText={this.state.filterText} inStockOnly={this.state.inStockOnly}/>
+      </div>
+    );
+  }
+}
+
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <FilterableProductTable products={PRODUCTS}/>
     );
   }
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,41 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import './index.css';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello, Daniel!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
-
 ReactDOM.render(
-  <Clock />,
-  document.getElementById('root')
+  <App />,
+  document.getElementById('container')
 );
